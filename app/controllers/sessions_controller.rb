9f76906264ca1d7262c12d31cf32f6b352dfedfa
marcosfparreiras <marcosfparreiras@gmail.com>
2017-05-06 17:19:07 -0300

    Redirect to Active Players on login; Change text using Players intead of User

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     user = Admin.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
-      redirect_to user
+      redirect_to active_players_path
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
