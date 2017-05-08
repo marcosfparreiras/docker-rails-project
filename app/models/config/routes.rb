4fb3532df3e01f731551a5aedc98608293ad1cf7
marcosfparreiras <marcosfparreiras@gmail.com>
2017-05-07 22:43:16 -0300

    Adjusts to deploy in production

--- app/models/config/routes.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.routes.draw do
-  resources :players
-  resources :plans
-  resources :admins
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-if [ "$RAILS_ENV" == "development" ]; then
+# if [ "$RAILS_ENV" == "development" ]; then
   # Wait for postgres
   while ! curl db:5432 2>&1 | grep '52'
   do
     sleep 1
   done
-fi
+  # Initialize Data Base
+  # bundle exec rails db:create
+  # bundle exec rails db:migrate
+  # bundle exec rails db:seed
+# fi
 
-# Initialize Data Base
-bundle exec rails db:create
-bundle exec rails db:migrate
-bundle exec rails db:seed
-
+rm -f ../tmp/pids/server.pid
 # Precompile
 if [ "$RAILS_ENV" == "production" ]; then
   bundle exec rails assets:precompile
-  bundle exec rails server -b 0.0.0.0 -p 80
+  bundle exec rails server -e production -b 0.0.0.0 -p 80
 fi
 # Run server
 
